3fe6f79355172e10db5a79cec74df5b9c996a773
Joe Elliott <joeboyscout04@gmail.com>
2017-09-04 20:13:08 +0200

    First API endpoints for just controlling lights

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask,make_response,jsonify
+from flask_restful import Api
+from lightsapi import LightsAPI
+
+#Plan out the API endpoints
+
+#/lights
+
+#Save favorite settings
+
+#Control by color
+
+#Control by arm?
+
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+#API Routes
+api.add_resource(LightsAPI, '/lights', endpoint = 'lights')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ lightsapi.py
@@ -0,0 +1,31 @@
+from flask_restful import Resource,reqparse
+import piglow
+
+
+class LightsAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('white', type=int, default=0, location='json')
+        self.reqparse.add_argument('red', type=int, default=0, location='json')
+        self.reqparse.add_argument('yellow', type=int, default=0, location='json')
+        self.reqparse.add_argument('orange', type=int, default=0, location='json')
+        self.reqparse.add_argument('blue', type=int, default=0, location='json')
+        self.reqparse.add_argument('green', type=int, default=0, location='json')
+        super(LightsAPI,self).__init__()
+
+    def get(self, id):
+        vals = piglow.get()
+        #make json from this
+
+        pass
+
+    def post(self, id):
+        args = self.reqparse.parse_args()
+        for k, v in args.iteritems():
+            piglow.colour(k,v)
+
+        piglow.show()
+        return
+        
+
+
